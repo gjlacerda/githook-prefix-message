a1a10d260e77765d7e23d7f5a24aef7db17badc2
gjlacerda <guilherme.lcd@gmail.com>
2017-11-20 21:36:42 -0200

    aff

--- githooks/commit-msg.js
+++ githooks/commit-msg.js
@@ -2,10 +2,13 @@ const sh   = require('shelljs');
 const fs   = require('fs');
 const util = require('util');
 
-const branchName      = sh.exec(`git branch | grep '^\*' | cut -b3-`, {silent: true}).stdout.replace('\n', '');
+const branchName = sh.exec(`git branch | grep '^\*' | cut -b3-`, {silent: true}).stdout.replace('\n', '');
+if (branchName === 'master') {
+    sh.exit(0);
+}
+
 const messageFilePath = process.env.PWD + '/.git/COMMIT_EDITMSG';
 const contents        = fs.readFileSync(messageFilePath);
 const message         = util.format('%s: %s', branchName, contents);
 
 fs.writeFileSync(messageFilePath, message);
-// teste
