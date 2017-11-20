dfbe3f0dbe2f4c9fde527c8cfe616528d744514b
gjlacerda <guilherme.lcd@gmail.com>
2017-11-20 21:28:38 -0200

    gui/teste: Final solution

--- githooks/commit-msg.js
+++ githooks/commit-msg.js
@@ -1,3 +1,10 @@
-#!/bin/sh
+const sh   = require('shelljs');
+const fs   = require('fs');
+const util = require('util');
 
-require('fs').writeFileSync(process.env.GIT_PARAMS, 'some message edited');
+const branchName      = sh.exec(`git branch | grep '^\*' | cut -b3-`, {silent: true}).stdout.replace('\n', '');
+const messageFilePath = process.env.PWD + '/.git/COMMIT_EDITMSG';
+const contents        = fs.readFileSync(messageFilePath);
+const message         = util.format('%s: %s', branchName, contents);
+
+fs.writeFileSync(messageFilePath, message);
